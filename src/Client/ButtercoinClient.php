727e2a6c486f913685d3ec64d8ea4cc3ed4cc13e
Kevin Adams <kevin@buttercoin.com>
2014-07-14 22:31:24 -0700

    added a bunch of new methods

--- src/Client/ButtercoinClient.php
+++ src/Client/ButtercoinClient.php
@@ -19,7 +19,7 @@ class ButtercoinClient extends Client
 	public static function factory ($config = array())
 	{
 		$default = array(
-			'baseUrl' => 'https://api.buttercoin.com/{version}/',
+			'baseUrl' => 'http://localhost:9002/{version}/',
 			'version' => 'v1',
 			'publicKey' => null,
 			'secretKey' => null
@@ -27,9 +27,9 @@ class ButtercoinClient extends Client
 
 		$config = Collection::fromConfig($config, $default);
 
-		$client - new self($config->get('baseUrl'), $config);
+		$client = new self($config->get('baseUrl'), $config);
 
-		$file = 'buttercoin' . str_replace('.', '_', $client->getConfig('version')) . '.php';
+		$file = 'buttercoin-' . str_replace('.', '_', $client->getConfig('version')) . '.php';
 		$client->setDescription(ServiceDescription::factory(__DIR__ . "/Resources/{$file}"));
 
 		// Set the content type header to use "application/json" for all requests
@@ -48,8 +48,8 @@ class ButtercoinClient extends Client
      */
     public function __call($method, $args)
     {
-        return $this->getCommand($method, isset($args[0]) ? $args[0] : array())->getResult();
-	}
+        return $this->getCommand($method, $args)->getResult();
+    }
 
 	/**
      * Sets the API Version used by the Buttercoin Client.
@@ -117,67 +117,16 @@ class ButtercoinClient extends Client
 	}
 
 	/**
-	 * set X-Buttercoin-Access-Key
-	 *
-	 * @param string $key
-	 */
-	public function setXButtercoinAccessKey($key)
-	{
-		return $this->getConfig()->set('X-Buttercoin-Access-Key', $key);
-	}
-
-	/**
-	 * get X-Buttercoin-Access-Key
-	 *
-	 * @return string|null value of X-Buttercoin-Access-Key
-	 */
-	public function getXButtercoinAccessKey()
-	{
-		return $this->getConfig('X-Buttercoin-Access-Key');
-	}
-
-	/**
-	 * set X-Buttercoin-Signature
-	 *
-	 * @param string $url the full url of the API call
-	 * @param integer $timestamp UNIX timestamp must be within 5 mins of Buttercoin UTC server time
-	 */
-	public function setXButtercoinSignature($url, $timestamp)
-	{
-		$tsUrl = $timestamp . $url;
-		$encodedUrl = base64_encode(utf8_encode($tsUrl));
-		$signature = base64_encode(hash_hmac('sha256', $encodedUrl, SECRET, TRUE));
-		return $this->getConfig()->set('X-Buttercoin-Signature', $signature);
-	}
-
-	/**
 	 * get X-Buttercoin-Signature
 	 *
 	 * @return string|null value of X-Buttercoin-Signature
 	 */
-	public function getXButtercoinSignature()
+	public function getXButtercoinSignature($url, $timestamp)
 	{
-		return $this->getConfig('X-Buttercoin-Signature');
-	}
-
-	/**
-	 * set X-Buttercoin-Date
-	 *
-	 * @param integer $timestamp UNIX timestamp must be within 5 mins of Buttercoin UTC server time
-	 */
-	public function setXButtercoinDate($timestamp)
-	{
-		return $this->getConfig()->set('X-Buttercoin-Date', $timestamp);
-	}
-
-	/**
-	 * get X-Buttercoin-Date
-	 *
-	 * @return string|null value of X-Buttercoin-Date
-	 */
-	public function getXButtercoinDate()
-	{
-		return $this->getConfig('X-Buttercoin-Date');
+		$tsUrl = $timestamp . $url;
+		$encodedUrl = base64_encode(utf8_encode($tsUrl));
+		$signature = base64_encode(hash_hmac('sha256', $encodedUrl, $this->getSecretKey(), TRUE));
+		return $signature;
 	}
 
 	/**
@@ -185,17 +134,55 @@ class ButtercoinClient extends Client
 	 *
 	 * @param string $command the command to use to create the URL
 	 * @params array $params the params to use for the method
+	 * @params boolean $handleParams set to true if params are part of the signed URL separately 
 	 *
 	 * return string the full url with properly formatted query params (json for POST, queryString for get)
 	 */
-	public function getUrl($command, $params)
+	public function getUrl($command, $params, $handleParams = true)
 	{
 		$url = str_replace('{version}', $this->getConfig('version'), $this->getConfig('baseUrl'));
-		$url .= $this->getCommand($command)['uri'];
+		$url .= $this->getCommand($command, [])->getClient()->getDescription()->getOperation($command)->getUri();
+		$url = str_replace('{id}', $params['id'], $url);
+
+		$httpMethod = $this->getCommand($command, [])->getClient()->getDescription()->getOperation($command)->getHttpMethod();
+
+		if (count($params) > 0 && $handleParams) {
+			if ($httpMethod === "GET") {
+				$url .= "?" . http_build_query($params);
+			} else if ($httpMethod === "POST") {
+				$url .= json_encode($params);
+			}
+		}
+		print_r($url . PHP_EOL);
 		
 		// TODO: logic for GET and POST requests to add query params if necessary
-
 		return $url;
+	}
+
+	/**
+	 * Shortcut for the getTicker command
+	 *
+	 * @param integer $timestamp UNIX timestamp must be within 5 mins of Buttercoin UTC server time
+	 *
+	 * @return mixed
+	 */
+	public function getTicker($timestamp)
+	{
+
+		return $this->buildCommand('getTicker', $timestamp);
+	}
+
+	/**
+	 * Shortcut for the getOrderBook command
+	 *
+	 * @param integer $timestamp UNIX timestamp must be within 5 mins of Buttercoin UTC server time
+	 *
+	 * @return mixed
+	 */
+	public function getOrderBook($timestamp)
+	{
+
+		return $this->buildCommand('getOrderBook', $timestamp);
 	}
 
 	/**
@@ -207,7 +194,6 @@ class ButtercoinClient extends Client
 	 */
 	public function getKey($timestamp)
 	{
-
 		return $this->buildCommand('getKey', $timestamp);
 	}
 
@@ -223,15 +209,81 @@ class ButtercoinClient extends Client
 		return $this->buildCommand('getBalances', $timestamp);
 	}
 
-	private function buildCommand($command, $timestamp, $params = [])
+	/**
+	 * Shortcut for the getDepositAddress command
+	 *
+	 * @param integer $timestamp UNIX timestamp must be within 5 mins of Buttercoin UTC server time
+	 *
+	 * @return mixed
+	 */
+	public function getDepositAddress($timestamp)
 	{
-		$this->setXButtercoinAccessKey($this->getConfig('publicKey'));
-		$this->setXButtercoinDate($timestamp);
-		$url = $this->getUrl($command, $params);
-		$this->setXButtercoinSignature($url, $timestamp);
-
-		return $this->getCommand($command)->getResult();
+		return $this->buildCommand('getDepositAddress', $timestamp);
 	}
-	
 
+	/**
+	 * Shortcut for the createOrder command
+	 *
+	 * @param string $orderId the UUIDv4 id of the order to get
+	 * @param integer $timestamp UNIX timestamp must be within 5 mins of Buttercoin UTC server time
+	 *
+	 * @return mixed
+	 */
+	public function getOrder($orderId, $timestamp )
+	{
+		return $this->buildCommand('getOrder', $timestamp, ["id" => $orderId], false);
+	}
+
+	/**
+	 * Shortcut for the createOrder command
+	 *
+	 * @param array $data array contain data for order creation (Please see Buttercoin docs)
+	 * @param integer $timestamp UNIX timestamp must be within 5 mins of Buttercoin UTC server time
+	 *
+	 * @return mixed
+	 */
+	public function getOrders($data, $timestamp)
+	{
+		$orders = $this->buildCommand('getOrders', $timestamp, $data);
+		return $orders['results'];
+	}
+
+	/**
+	 * Shortcut for the createOrder command
+	 *
+	 * @param array $data array contain data for order creation (Please see Buttercoin docs)
+	 * @param integer $timestamp UNIX timestamp must be within 5 mins of Buttercoin UTC server time
+	 *
+	 * @return mixed
+	 */
+	public function createOrder($data, $timestamp)
+	{
+		return $this->buildCommand('createOrder', $timestamp, $data);
+	}
+
+	/**
+	 * Shortcut for the deleteOrder command
+	 *
+	 * @param string $orderId the UUIDv4 id of the order to delete
+	 * @param integer $timestamp UNIX timestamp must be within 5 mins of Buttercoin UTC server time
+	 *
+	 * @return mixed
+	 */
+	public function deleteOrder($orderId, $timestamp )
+	{
+		return $this->buildCommand('deleteOrder', $timestamp, ["id" => $orderId], false);
+	}
+
+	private function buildCommand($command, $timestamp, $params = [], $handleParams = true)
+	{
+		$url = $this->getUrl($command, $params, $handleParams);
+
+		$params['X-Buttercoin-Access-Key'] = $this->getConfig('publicKey');
+		$params['X-Buttercoin-Date'] = "$timestamp";
+		$params['X-Buttercoin-Signature'] = $this->getXButtercoinSignature($url, $timestamp);
+		$params['X-Forwarded-For'] = '127.0.0.1';
+		print_r($params);
+		$this->getConfig()->set('command.params', $params);
+		return $this->getCommand($command, $params)->getResult();
+	}
 }
--- src/Client/Resources/buttercoin-v1.php
+++ src/Client/Resources/buttercoin-v1.php
@@ -4,65 +4,303 @@ return array(
     'name'			=> 'Buttercoin',
     'apiVersion'	=> 'v1',
 	'operations'	=> array(
-		'getKey'	=> array(
-			'uri'			=> 'key',
+		'getTicker' 	=> array(
+			'uri'		=> 'ticker',
+			'description'	=> 'get the latest bid, ask, and trade price',
+			'httpMethod'	=> 'GET'
+		),
+		'getOrderBook' 	=> array(
+			'uri'		=> 'orderbook',
+			'description'	=> 'get all the current open bids and asks in the order book',
+			'httpMethod'	=> 'GET'
+		),
+		'getKey'		=> array(
+			'uri'		=> 'key',
 			'description'	=> 'get the permissions associated with this API Key',
 			'httpMethod'	=> 'GET',
 			'parameters'	=> array(
-				'xButtercoinAccessKey' => array (
+				'X-Buttercoin-Access-Key' => array (
 					'location'		=> 'header',
-					'description'	=> 'Your Public API Key',
+					'description'		=> 'Your Public API Key',
 					'sentAs'		=> 'X-Buttercoin-Access-Key',
 					'pattern'		=> '/^([[:alnum:]])+$/',
 					'type'			=> 'string',
 					'required'		=> true
 				),
-				'xButtercoinSignature' => array (
+				'X-Buttercoin-Signature' => array (
 					'location'		=> 'header',
-					'description'	=> 'HMAC-SHA256 signature',
+					'description'		=> 'HMAC-SHA256 signature',
 					'sentAs'		=> 'X-Buttercoin-Signature',
-					'pattern'		=> '/^([[:alnum:]])+$/',
 					'type'			=> 'string',
 					'required'		=> true
 				),
-				'xButtercoinDate' => array (
+				'X-Buttercoin-Date' => array (
 					'location'		=> 'header',
-					'description'	=> 'Unix Timestamp must be within 5 minutes of Buttercoin UTC server time',
+					'description'		=> 'Unix Timestamp must be within 5 minutes of Buttercoin UTC server time',
 					'sentAs'		=> 'X-Buttercoin-Date',
-					'pattern'		=> '/^([[:num:]])+$/',
 					'type'			=> 'string',
 					'required'		=> true
 				)
 			)
 		),
 		'getBalances' => array(
-			'uri'			=> 'balances',
+			'uri'		=> 'account/balances',
 			'description'	=> 'get the balances associated with this account',
 			'httpMethod'	=> 'GET',
 			'parameters'	=> array(
-				'xButtercoinAccessKey' => array (
+				'X-Buttercoin-Access-Key' => array (
 					'location'		=> 'header',
-					'description'	=> 'Your Public API Key',
+					'description'		=> 'Your Public API Key',
 					'sentAs'		=> 'X-Buttercoin-Access-Key',
 					'pattern'		=> '/^([[:alnum:]])+$/',
 					'type'			=> 'string',
 					'required'		=> true
 				),
-				'xButtercoinSignature' => array (
+				'X-Buttercoin-Signature' => array (
 					'location'		=> 'header',
-					'description'	=> 'HMAC-SHA256 signature',
+					'description'		=> 'HMAC-SHA256 signature',
 					'sentAs'		=> 'X-Buttercoin-Signature',
+					'type'			=> 'string',
+					'required'		=> true
+				),
+				'X-Buttercoin-Date' => array (
+					'location'		=> 'header',
+					'description'		=> 'Unix Timestamp must be within 5 minutes of Buttercoin UTC server time',
+					'sentAs'		=> 'X-Buttercoin-Date',
+					'type'			=> 'string',
+					'required'		=> true
+				)
+			)
+			
+		),
+		'getDepositAddress'	=> array(
+			'uri'			=> 'account/depositAddress',
+			'description'		=> 'get the bitcoin deposit address for this account',
+			'httpMethod'		=> 'GET',
+			'parameters'		=> array(
+				'X-Buttercoin-Access-Key' => array (
+					'location'		=> 'header',
+					'description'		=> 'Your Public API Key',
+					'sentAs'		=> 'X-Buttercoin-Access-Key',
 					'pattern'		=> '/^([[:alnum:]])+$/',
 					'type'			=> 'string',
 					'required'		=> true
 				),
-				'xButtercoinDate' => array (
+				'X-Buttercoin-Signature' => array (
 					'location'		=> 'header',
-					'description'	=> 'Unix Timestamp must be within 5 minutes of Buttercoin UTC server time',
-					'sentAs'		=> 'X-Buttercoin-Date',
-					'pattern'		=> '/^([[:num:]])+$/',
+					'description'		=> 'HMAC-SHA256 signature',
+					'sentAs'		=> 'X-Buttercoin-Signature',
 					'type'			=> 'string',
 					'required'		=> true
+				),
+				'X-Buttercoin-Date' 	=> array (
+					'location'		=> 'header',
+					'description'		=> 'Unix Timestamp must be within 5 minutes of Buttercoin UTC server time',
+					'sentAs'		=> 'X-Buttercoin-Date',
+					'type'			=> 'string',
+					'required'		=> true
+				)
+			)
+		),
+		'getOrder' => array(
+			'uri'		=> 'orders/{id}',
+			'description'	=> 'get order by id',
+			'httpMethod'	=> 'GET',
+			'parameters'	=> array(
+				'X-Buttercoin-Access-Key' => array (
+					'location'		=> 'header',
+					'description'		=> 'Your Public API Key',
+					'sentAs'		=> 'X-Buttercoin-Access-Key',
+					'pattern'		=> '/^([[:alnum:]])+$/',
+					'type'			=> 'string',
+					'required'		=> true
+				),
+				'X-Buttercoin-Signature' => array (
+					'location'		=> 'header',
+					'description'		=> 'HMAC-SHA256 signature',
+					'sentAs'		=> 'X-Buttercoin-Signature',
+					'type'			=> 'string',
+					'required'		=> true
+				),
+				'X-Buttercoin-Date' => array (
+					'location'		=> 'header',
+					'description'		=> 'Unix Timestamp must be within 5 minutes of Buttercoin UTC server time',
+					'sentAs'		=> 'X-Buttercoin-Date',
+					'type'			=> 'string',
+					'required'		=> true
+				),
+				'id'       => array(
+					'location' 		=> 'uri',
+					'type'     		=> 'string',
+					'description' 		=> 'UUIDv4 id of the order to get',
+					'required' 		=> true
+				)
+			)
+			
+		),
+		'getOrders' => array(
+			'uri'		=> 'orders',
+			'description'	=> 'get orders',
+			'httpMethod'	=> 'GET',
+			'parameters'	=> array(
+				'X-Buttercoin-Access-Key' => array (
+					'location'		=> 'header',
+					'description'		=> 'Your Public API Key',
+					'sentAs'		=> 'X-Buttercoin-Access-Key',
+					'pattern'		=> '/^([[:alnum:]])+$/',
+					'type'			=> 'string',
+					'required'		=> true
+				),
+				'X-Buttercoin-Signature' => array (
+					'location'		=> 'header',
+					'description'		=> 'HMAC-SHA256 signature',
+					'sentAs'		=> 'X-Buttercoin-Signature',
+					'type'			=> 'string',
+					'required'		=> true
+				),
+				'X-Buttercoin-Date' 	=> array (
+					'location'		=> 'header',
+					'description'		=> 'Unix Timestamp must be within 5 minutes of Buttercoin UTC server time',
+					'sentAs'		=> 'X-Buttercoin-Date',
+					'type'			=> 'string',
+					'required'		=> true
+				),
+				'status'       		=> array(
+					'location' 		=> 'query',
+					'description'		=> 'opened, reopened, filled, or canceled',
+					'type'     		=> 'string',
+					'required'		=> false
+				),
+				'side'       		=> array(
+					'location' 		=> 'query',
+					'description'		=> 'buy or sell',
+					'type'     		=> 'string',
+					'required'		=> false
+				),
+				'orderType'       	=> array(
+					'location' 		=> 'query',
+					'description'		=> 'market or limit',
+					'type'     		=> 'string',
+					'required'		=> false
+				),
+				'dateMin'       	=> array(
+					'location' 		=> 'query',
+					'description'		=> 'ISO 8601 Date-Time',
+					'type'     		=> 'string',
+					'required'		=> false
+				),
+				'dateMax'       	=> array(
+					'location' 		=> 'query',
+					'description'		=> 'ISO 8601 Date-Time',
+					'type'     		=> 'string',
+					'required'		=> false
+				)
+				
+				
+				
+				
+			)
+			
+		),
+		'createOrder' => array(
+			'uri'		=> 'orders',
+			'description'	=> 'create a new order',
+			'httpMethod'	=> 'POST',
+			'parameters'	=> array(
+				'X-Buttercoin-Access-Key' => array (
+					'location'		=> 'header',
+					'description'		=> 'Your Public API Key',
+					'sentAs'		=> 'X-Buttercoin-Access-Key',
+					'pattern'		=> '/^([[:alnum:]])+$/',
+					'type'			=> 'string',
+					'required'		=> true
+				),
+				'X-Buttercoin-Signature' => array (
+					'location'		=> 'header',
+					'description'		=> 'HMAC-SHA256 signature',
+					'sentAs'		=> 'X-Buttercoin-Signature',
+					'type'			=> 'string',
+					'required'		=> true
+				),
+				'X-Buttercoin-Date' 	=> array (
+					'location'		=> 'header',
+					'description'		=> 'Unix Timestamp must be within 5 minutes of Buttercoin UTC server time',
+					'sentAs'		=> 'X-Buttercoin-Date',
+					'type'			=> 'string',
+					'required'		=> true
+				),
+				'instrument'       	=> array(
+					'location' 		=> 'json',
+					'description'		=> 'The two assets being traded (e.g. BTC_USD or USD_BTC)',
+					'type'     		=> 'string',
+					'required'		=> true
+				),
+				'side'       		=> array(
+					'location' 		=> 'json',
+					'description'		=> 'buy or sell',
+					'type'     		=> 'string',
+					'required'		=> true
+				),
+				'orderType'       	=> array(
+					'location' 		=> 'json',
+					'description'		=> 'market or limit',
+					'type'     		=> 'string',
+					'required'		=> true
+				),
+				'price'       		=> array(
+					'location' 		=> 'json',
+					'description'		=> 'the amount paid for the asset',
+					'type'     		=> 'string',
+					'required'		=> true
+				),
+				'quantity' 	      	=> array(
+					'location' 		=> 'json',
+					'description'		=> 'the quantity of asset desired (required for limit orders)',
+					'type'     		=> 'string',
+					'required'		=> false
+				)
+			)
+			
+		),
+		'deleteOrder' => array(
+			'uri'		=> 'orders/{id}',
+			'description'	=> 'get order by id',
+			'httpMethod'	=> 'DELETE',
+			'parameters'	=> array(
+				'X-Buttercoin-Access-Key' => array (
+					'location'		=> 'header',
+					'description'		=> 'Your Public API Key',
+					'sentAs'		=> 'X-Buttercoin-Access-Key',
+					'pattern'		=> '/^([[:alnum:]])+$/',
+					'type'			=> 'string',
+					'required'		=> true
+				),
+				'X-Buttercoin-Signature' => array (
+					'location'		=> 'header',
+					'description'		=> 'HMAC-SHA256 signature',
+					'sentAs'		=> 'X-Buttercoin-Signature',
+					'type'			=> 'string',
+					'required'		=> true
+				),
+				'X-Buttercoin-Date' 	=> array (
+					'location'		=> 'header',
+					'description'		=> 'Unix Timestamp must be within 5 minutes of Buttercoin UTC server time',
+					'sentAs'		=> 'X-Buttercoin-Date',
+					'type'			=> 'string',
+					'required'		=> true
+				),
+				'X-Forwarded-For' 	=> array (
+					'location'		=> 'header',
+					'description'		=> 'localhost ip',
+					'sentAs'		=> 'X-Forwarded-For',
+					'type'			=> 'string',
+					'required'		=> true
+				),
+				'id'       		=> array(
+					'location' => 'uri',
+					'type'     => 'string',
+					'description' => 'UUIDv4 id of the order to get',
+					'required' => true
 				)
 			)
 			
